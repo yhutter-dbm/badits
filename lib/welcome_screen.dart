fc09e583147ae39cb13d2cb5d05e668e35010de0
YahArt <hutteryannick@gmail.com>
2020-10-15 11:11:58 +0200

    Added dummy welcome screen

---
+++ lib/welcome_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  Future<void> _simulateDataLoading() {
+    // TODO: Add any data loading which needs to happen before starting the app here...
+    return Future.delayed(Duration(seconds: 3));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: _simulateDataLoading(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            // We are finished fetching any startup data here...
+            return Container(child: Text('Finished...'));
+          }
+          return Scaffold(
+            body: Container(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  'Habit Reminder',
+                  style: TextStyle(fontFamily: 'TitleFont', fontSize: 35),
+                  textAlign: TextAlign.center,
+                ),
+                Image(image: AssetImage('images/welcome_screen.png'))
+              ],
+            )),
+          );
+        });
+  }
+}
