3c96e938c09b2cd9cce893f821b0c5b744fa8941
YahArt <hutteryannick@gmail.com>
2020-10-16 10:11:25 +0200

    Redesigned Habit UI on dashboard

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -2,6 +2,7 @@ import 'package:badits/models/habit.dart';
 import 'package:badits/services/service_locator.dart';
 import 'package:badits/services/storage_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_emoji/flutter_emoji.dart';
 import 'package:intl/intl.dart';
 
 import 'create_habit_dialog.dart';
@@ -13,6 +14,7 @@ class DashboardScreen extends StatefulWidget {
 
 class _DashboardScreenState extends State<DashboardScreen> {
   final _dateFormat = 'dd.MM.yyyy';
+  final _emojiParser = EmojiParser();
 
   String _getFormattedDate(DateTime date) {
     return DateFormat(_dateFormat).format(date);
@@ -38,9 +40,48 @@ class _DashboardScreenState extends State<DashboardScreen> {
     setState(() {});
   }
 
+  List<Widget> _generateHabitList(List<Habit> habits) {
+    return List.generate(habits.length, (index) {
+      final habit = habits[index];
+      return Card(
+        child: Container(
+          padding: EdgeInsets.all(5),
+          child: Stack(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    habit.name,
+                    style: Theme.of(context).textTheme.headline5,
+                  ),
+                  Text(
+                    '${_getFormattedDate(habit.dueDate)}',
+                    style: TextStyle(fontStyle: FontStyle.italic),
+                  ),
+                  Text(habit.description),
+                ],
+              ),
+              Positioned(
+                  bottom: 0,
+                  right: 0,
+                  child: Row(
+                    children: [
+                      FlatButton(onPressed: null, child: Text('Not Completed')),
+                      FlatButton(onPressed: null, child: Text('Completed'))
+                    ],
+                  ))
+            ],
+          ),
+        ),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           _showCreateHabitDialog(context);
@@ -59,26 +100,36 @@ class _DashboardScreenState extends State<DashboardScreen> {
         future: _getHabitsFromStorage(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
+            final List<Habit> habits = snapshot.data;
             return Container(
-              child: ListView.builder(
-                itemCount: snapshot.data.length,
-                itemBuilder: (context, index) {
-                  final Habit habit = snapshot.data[index];
-                  return ListTile(
-                      title: Text(habit.name),
-                      subtitle: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            '${_getFormattedDate(habit.dueDate)}',
-                            style: TextStyle(fontStyle: FontStyle.italic),
+                padding: EdgeInsets.all(5),
+                child: GridView.count(
+                    mainAxisSpacing: 5,
+                    crossAxisCount: 1,
+                    children: [
+                      Card(
+                        child: Container(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text(
+                                'Rock it today!',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                '10/10 completed',
+                                style: Theme.of(context).textTheme.headline3,
+                              ),
+                              Text(
+                                _emojiParser.emojify(':muscle:'),
+                                style: Theme.of(context).textTheme.headline1,
+                              )
+                            ],
                           ),
-                          Text(habit.description),
-                        ],
-                      ));
-                },
-              ),
-            );
+                        ),
+                      ),
+                      ..._generateHabitList(habits)
+                    ]));
           }
           return Container();
         },
