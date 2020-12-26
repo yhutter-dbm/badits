ee9417f8866550b1bd5f2798c29f9b000746f2ea
YahArt <hutteryannick@gmail.com>
2020-12-26 08:52:52 +0100

    Sort habits according to id

--- lib/extensions/habit_list_extension.dart
+++ lib/extensions/habit_list_extension.dart
@@ -2,25 +2,8 @@ import 'package:badits/models/habit.dart';
 
 extension HabitList on List<Habit> {
   void sortHabits() {
-    final now = DateTime.now();
-    // Sort according to dueDate
-    this.sort((Habit habitOne, Habit habitTwo) =>
-        habitOne.dueDate.compareTo(habitTwo.dueDate));
-    // Filter out all completed habits
-    final completedForToday =
-        this.where((Habit habit) => habit.completedForToday).toList();
-
-    final passDue =
-        this.where((Habit habit) => habit.isPassDueDate(now)).toList();
-
-    final others = this
-        .where((Habit habit) =>
-            !completedForToday.contains(habit) && !passDue.contains(habit))
-        .toList();
-
-    this.clear();
-    this.addAll(others);
-    this.addAll(completedForToday);
-    this.addAll(passDue);
+    // Sort according to habit id, this ensures that the newest added habit will always be first
+    this.sort(
+        (Habit habitOne, Habit habitTwo) => habitTwo.id.compareTo(habitOne.id));
   }
 }
--- test/models/habit_test.dart
+++ test/models/habit_test.dart
@@ -4,7 +4,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:badits/extensions/habit_list_extension.dart';
 
 void main() {
-  test('Should sort habits descending according to due date', () {
+  test('Should sort habits descending according to habit id', () {
     final now = DateTime.now();
     final Habit habitOne = Habit(
         id: 0,
@@ -42,116 +42,11 @@ void main() {
         currentCompletionCount: 0,
         countUntilCompletion: 3);
 
-    final List<Habit> testData = [habitOne, habitTwo, habitThree];
+    final List<Habit> testData = [habitThree, habitOne, habitTwo];
 
     testData.sortHabits();
 
-    final List<Habit> expectedSorting = [habitTwo, habitThree, habitOne];
-    expect(testData, expectedSorting);
-  });
-
-  test('Should sort pass due habits always last', () {
-    // Pass due is when the dueDate is older then the current date
-    final now = DateTime.now();
-    final Habit habitPassDue = Habit(
-        id: 0,
-        name: 'habitPassDue',
-        creationDate: now,
-        nextCompletionDate: now.add(Duration(days: 1)),
-        dueDate: now.subtract(Duration(days: 3)),
-        assetIcon: '',
-        duration: HabitDuration.daily,
-        completedForToday: false,
-        currentCompletionCount: 0,
-        countUntilCompletion: 3);
-
-    final Habit habitOne = Habit(
-        id: 1,
-        name: 'habitOne',
-        creationDate: now,
-        nextCompletionDate: now.add(Duration(days: 1)),
-        dueDate: now.add(Duration(days: 2)),
-        assetIcon: '',
-        duration: HabitDuration.daily,
-        completedForToday: false,
-        currentCompletionCount: 0,
-        countUntilCompletion: 2);
-
-    final Habit habitTwo = Habit(
-        id: 2,
-        name: 'habitTwo',
-        creationDate: now,
-        nextCompletionDate: now.add(Duration(days: 1)),
-        dueDate: now.add(Duration(days: 3)),
-        assetIcon: '',
-        duration: HabitDuration.daily,
-        completedForToday: false,
-        currentCompletionCount: 0,
-        countUntilCompletion: 3);
-
-    final List<Habit> testData = [habitPassDue, habitOne, habitTwo];
-
-    testData.sortHabits();
-
-    // Now the habits should be sorting descending from the duedate (the earliest one is the first element etc.)
-    final List<Habit> expectedSorting = [habitOne, habitTwo, habitPassDue];
-    expect(testData, expectedSorting);
-  });
-
-  test(
-      'Should sort habits which are completed for today after incomplete but before pass due ones',
-      () {
-    // Pass due is when the dueDate is older then the current date
-    final now = DateTime.now();
-    final Habit habitPassDue = Habit(
-        id: 0,
-        name: 'habitPassDue',
-        creationDate: now,
-        nextCompletionDate: now.add(Duration(days: 1)),
-        dueDate: now.subtract(Duration(days: 3)),
-        assetIcon: '',
-        duration: HabitDuration.daily,
-        completedForToday: false,
-        currentCompletionCount: 0,
-        countUntilCompletion: 3);
-
-    final Habit habitOne = Habit(
-        id: 1,
-        name: 'habitOne',
-        creationDate: now,
-        nextCompletionDate: now.add(Duration(days: 1)),
-        dueDate: now.add(Duration(days: 5)),
-        assetIcon: '',
-        duration: HabitDuration.daily,
-        completedForToday: false,
-        currentCompletionCount: 0,
-        countUntilCompletion: 5);
-
-    final Habit habitCompletedForToday = Habit(
-        id: 2,
-        name: 'habitCompletedForToday',
-        creationDate: now,
-        nextCompletionDate: now.add(Duration(days: 1)),
-        dueDate: now.add(Duration(days: 3)),
-        assetIcon: '',
-        duration: HabitDuration.daily,
-        completedForToday: true,
-        currentCompletionCount: 0,
-        countUntilCompletion: 3);
-
-    final List<Habit> testData = [
-      habitPassDue,
-      habitOne,
-      habitCompletedForToday
-    ];
-
-    testData.sortHabits();
-
-    final List<Habit> expectedSorting = [
-      habitOne,
-      habitCompletedForToday,
-      habitPassDue
-    ];
+    final List<Habit> expectedSorting = [habitThree, habitTwo, habitOne];
     expect(testData, expectedSorting);
   });
 }
